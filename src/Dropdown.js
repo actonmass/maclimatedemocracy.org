8b1c228931cca9547637a6e3a4b868841e09679b
Florian Pagnoux <florian.pagnoux@gmail.com>
2020-06-21 21:15:50 -0400

    Add dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
+import { Dropdown } from "semantic-ui-react";
 
-function DistrictDropdown() {
-  return <h1>Coucouch</h1>
+function DistrictDropdown({ data }) {
+  const options = data.map(({ label, value }) => {
+    return {
+      text: label,
+      value,
+      key: value,
+    };
+  });
+
+  return (
+    <div className="cbox">
+      <Dropdown
+        placeholder="Please select your district"
+        search
+        selection
+        upward={false}
+        options={options}
+        onChange={(event, data) => {goToForm(data.value)}}
+      />
+    </div>
+  );
+
+}
+function goToForm(districtId) {
+  const newUrl = `/2020BQ/${districtId}/Climate/`;
+  window.location.href = newUrl;
 }
 
 function renderDropdown(targetID, data) {
